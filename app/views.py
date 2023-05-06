9cd78a69b1504258c846d0275d88e8256c15d428
brayancristancho <brayan.cristanchof@gmail.comm>
2023-05-06 13:08:49 -0500

    configurations in template and new venv

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'html/jinja2/index.j2')
+    return render(request, 'html/jinja2/index.j2') 
