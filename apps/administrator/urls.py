1b20b2bde656ddc54d670faaede979d06a27382a
msebasv <msebasv@outlook.com>
2023-05-26 14:14:04 -0500

    Edit urls and add redirect to sidebar

--- apps/administrator/urls.py
+++ apps/administrator/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 from .views import CreateAct, ListAct
 
 urlpatterns = [
-    path('new_act/', login_required(CreateAct.as_view())),
-    path('list_act/', login_required(ListAct.as_view())),
+    path('new_act/', login_required(CreateAct.as_view()), name="new_act"),
+    path('list_act/', login_required(ListAct.as_view()), name="list_act"),
 ]
