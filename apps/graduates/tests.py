b6c9af89bccea082e6e4761961f983a97ebf8428
Joseph_fps <jfsolanop@unbosque.edu.co>
2023-06-04 01:42:40 -0500

    add event and test forms (don't touch test)

--- apps/graduates/tests.py
+++ apps/graduates/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from apps.student.models import Student
+from .models import graduates
 
-# Create your tests here.
+class GraduateModelTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(name="John Doe", age=25)
+        self.graduates = graduates.objects.create(
+            salary_range='1',
+            email='john@example.com',
+            previous_email='previous@example.com',
+            achievement_level='High',
+            work_experience=3,
+            cv='cv/example.pdf',
+            job_id=None,
+            student_id=self.student
+        )
+
+    def test_model_fields(self):
+        self.assertEqual(self.graduates.salary_range, '1')
+        self.assertEqual(self.graduates.email, 'john@example.com')
+        self.assertEqual(self.graduates.previous_email, 'previous@example.com')
+        self.assertEqual(self.graduates.achievement_level, 'High')
+        self.assertEqual(self.graduates.work_experience, 3)
+        self.assertEqual(self.graduates.cv, 'cv/example.pdf')
+        self.assertIsNone(self.graduates.job_id)
+        self.assertEqual(self.graduates.student_id, self.student)
+
+    def test_model_table_name(self):
+        self.assertEqual(self.graduates._meta.db_table, 'Graduate')
--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from user.forms import CustomUserCreationForm, FormLogin
 
-# Create your tests here.
+class FormTestCase(TestCase):
+    def test_custom_user_creation_form(self):
+        # Crea datos de prueba para el formulario
+        form_data = {
+            'username': 'testuser',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+
+        # Crea una instancia del formulario con los datos de prueba
+        form = CustomUserCreationForm(data=form_data)
+
+        # Verifica que el formulario sea válido
+        self.assertTrue(form.is_valid())
+
+        # Guarda el usuario creado por el formulario
+        user = form.save()
+
+        # Verifica que el usuario se haya guardado correctamente
+        self.assertEqual(User.objects.count(), 1)
+        saved_user = User.objects.first()
+        self.assertEqual(saved_user.username, 'testuser')
+
+        # Verifica que la contraseña se haya encriptado correctamente
+        self.assertNotEqual(saved_user.password, 'testpassword')
+
+    def test_form_login(self):
+        # Crea datos de prueba para el formulario
+        form_data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+        }
+
+        # Crea una instancia del formulario con los datos de prueba
+        form = FormLogin(data=form_data)
+
+        # Verifica que el formulario sea válido
+        self.assertTrue(form.is_valid())
+
+        # Verifica que los atributos del campo username se hayan configurado correctamente
+        self.assertEqual(form.fields['username'].widget.attrs['class'], 'form-control')
+        self.assertEqual(form.fields['username'].widget.attrs['placeholder'], 'Usuario')
+
+        # Verifica que los atributos del campo password se hayan configurado correctamente
+        self.assertEqual(form.fields['password'].widget.attrs['class'], 'form-control')
+        self.assertEqual(form.fields['password'].widget.attrs['placeholder'], 'Contraseña')
