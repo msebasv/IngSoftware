0a0d6476d0b61223223658f0ee33664a54b4850d
Brayan-Camilo-Cristancho-Franco <81502218+Brayan-Camilo-Cristancho-Franco@users.noreply.github.com>
2023-06-09 12:42:03 -0500

    Update settings.py

--- sieunbosque/settings.py
+++ sieunbosque/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@jg^4bx3p53dbass49t&*5f7sw4amqqf31o7biwn82!_kh54ve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#Tengo miedo
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['sieaunbosque.azurewebsites.net']
